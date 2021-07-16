ba2a0eadf1ad81fd8ff4e419c4e270adcda9ee4e
Mohammad Iqbal <mohiqbal954@gmail.com>
2021-07-16 18:20:01 -0400

    Completed inital structure of website

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,14 +1,20 @@
-import './menu.scss'
+  
+import "./menu.scss";
 
-export default function Menu({menuOpen, setMenuOpen}) {
-    return (
-        <div className={"menu " + (menuOpen && "active")}>
-            <ul>
-                <li onClick={()=>setMenuOpen(false)}><a href="#intro">Home</a></li>
-                <li onClick={()=>setMenuOpen(false)}><a href="#portfolio">Portfolio</a></li>
-                <li onClick={()=>setMenuOpen(false)}><a href="#work">Work</a></li>
-            </ul>
-
-        </div>
-    )
+export default function Menu({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"menu "+(menuOpen && "active")}>
+      <ul>
+        <li onClick={()=>setMenuOpen(false)}>
+          <a href="#intro">Home</a>
+        </li>
+        <li onClick={()=>setMenuOpen(false)}>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li onClick={()=>setMenuOpen(false)}>
+          <a href="#works">Works</a>
+        </li>
+      </ul>
+    </div>
+  );
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,45 +1,75 @@
+import { useState } from "react";
 import "./portfolio.scss"
-import PortfolioList from "../portfolioList/PortfolioList";
 
 export default function Portfolio() {
-    const list = [
+    const [currentSlide, setCurrentSlide] = useState(0)
+    const data = [
         {
-            id: "featured",
-            title: "Featured",
+          id: "1",
+          icon: "./assets/github-logo.png",
+          title: "Best-By App",
+          desc:
+            "Web based grocery management system focusing on food waste prevention.",
+          img:
+            "./assets/best-by-add.png",
         },
         {
-            id: "cinema-app",
-            title: "Cinema App",
+          id: "2",
+          icon: "./assets/github-logo.png",
+          title: "Autonomous Air Hockey Table",
+          desc:
+            "Algorithm that takes start and end coordinates and charts optimal path, while avoiding specifed obstacles.",
+          img:
+            "./assets/care-hockey.png",
         },
         {
-            id: "portfolio-website",
-            title: "Portfolio Website",
+          id: "3",
+          icon: "./assets/github-logo.png",
+          title: "Path Planning for Robotic Manipulator",
+          desc:
+            "Algorithm that takes start and end coordinates and charts optimal path, while avoiding specifed obstacles.",
+          img:
+            "./assets/manipulator-path.png",
         },
-        {
-            id: "path-planning",
-            title: "Robotic Arm Path Planning",
-        },
-        {
-            id: "air-hockey",
-            title: "Autonomous Air Hockey Table",
-        }
-        
-    ];
+      ];
+   
+    const handleClick = dir => {
+        dir === "left" ? 
+            setCurrentSlide(currentSlide > 0 ? currentSlide - 1: 2)
+            : setCurrentSlide(currentSlide< data.length - 1 ? currentSlide + 1 : 0)
+    }
+    
     return (
         <div className="portfolio" id="portfolio">
-            <h1>Portfolio</h1>
-            <ul>
-                {list.map( item => (
-                    <PortfolioList title={item.title} id={item.id} />
-                ))}
-            </ul>
+           <div 
+            className="slider"
+            style={{transform: `translateX(-${currentSlide *100}vw)`}}
+            > 
+            {data.map( d => (
+                <div className="container">
+                    <div className="item">
+                        <div className="left">
+                            <div className="leftContainer">
+                                <div className="imgContainer">
+                                    <img src={d.icon} alt="icon" />
+                                </div>
+                                <h2>{d.title}</h2>
+                                <p>{d.desc}</p>
+                                <span>Technologies Used</span>
+                            </div>
+                        </div>
+                        <div className="right">
+                            <div className="displayContainer">
+                              <img src={d.img} alt="logo" />
+                            </div>
+                        </div>
+                        
+                    </div>
 
-            <div className="container">
-                <div className="item">
-                    <img src="" alt="" />
-                    
-                </div>
-            </div>
+                </div>))}
+           </div>
+           <img src="assets/arrow.png" className="arrow-left" alt="left arrow" onClick={() => handleClick("left")} />
+            <img src="assets/arrow.png" className="arrow-right" alt="right arrow" onClick={() => handleClick("right")}/>    
         </div>
     )
 }
